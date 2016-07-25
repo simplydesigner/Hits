aa80acf6cbc6cfaacde694718a1aac58eedf4989
simplydesigner <simplydesigner@ya.ru>
2016-07-25 14:06:06 +0300

    Initialized

--- Hits/main.cpp
+++ Hits/main.cpp
@@ -1,15 +1,66 @@
-//
-//  main.cpp
-//  Hits
-//
-//  Created by simplydesigner on 7/25/16.
-//  Copyright © 2016 simplydesigner. All rights reserved.
-//
-
 #include <iostream>
 
+class Widget
+{
+    
+};
+
+const Widget & operator++(const Widget &lhs)// lvalue-ссылка может быть инициализирована rvalue-объектом, если только она не является константной
+{
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    return lhs;
+}
+
+int f1(int param)
+{
+    return param;
+}
+Widget f1(Widget param)
+{
+    return param;
+}
+
+int & f2(int & param)
+{
+    return param;
+}
+Widget & f2(Widget & param)
+{
+    return param;
+}
+
+int && f3(int && param)
+{
+    return std::move(param);
+}
+Widget && f3(Widget && param)
+{
+    return std::move(param);
+}
+
+int * f4(int param)
+{
+    return new int(param);
+}
+Widget * f4(Widget param)
+{
+    return new Widget(param);
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int value = 0;
+    ++f1(value);
+    ++f2(value);
+    ++f3(std::move(value));
+    ++f4(value);
+    
+    Widget widget;
+    ++f1(widget);
+    ++f2(widget);
+    ++f3(std::move(widget));
+    ++f4(widget);
+    
+    
     return 0;
 }
